b4268156c7f82ea83de4c8ef43c6974598b875ea
Renan <renan.gonela@unesp.br>
2022-06-09 10:39:28 -0300

    first comit

---
+++ R/somar.R
@@ -0,0 +1,16 @@
+#'Função para somar
+#'
+#'Esta função será utilizada para somar dois numeros
+#'
+#' @param a um numero
+#' @param b outro numero
+#'
+#' @examples
+#' somar(3, 4)
+#'
+#'
+#' @export
+somar<-function(a, b){
+  a + b
+}
+
---
+++ R/subtrair.R
@@ -0,0 +1,15 @@
+#'Função para subtrair
+#'
+#'Esta função será utilizada para subtrair dois numeros
+#'
+#' @param a um numero
+#' @param b outro numero
+#'
+#' @examples
+#' subtrair(3, 4)
+#'
+#'
+#' @export
+subtrair<-function(a, b){
+  a - b
+}
