bb6fc59b61094315e57cd1cab0cb99f5ad8b95f5
Renan <renan.gonela@unesp.br>
2022-06-09 11:50:04 -0300

    third comit

---
+++ R/dividir.R
@@ -0,0 +1,15 @@
+#'Função para dividir
+#'
+#'Esta função será utilizada para dividir dois numeros
+#'
+#' @param a um numero
+#' @param b outro numero
+#'
+#' @examples
+#' Dividir(8, 4)
+#'
+#'
+#' @export
+dividir<-function(a, b){
+  a / b
+}
