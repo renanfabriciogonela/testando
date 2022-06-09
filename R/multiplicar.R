8a865327a68af90802b46cf85f19cbfc0d31b83f
Renan <renan.gonela@unesp.br>
2022-06-09 11:39:00 -0300

    second comit

---
+++ R/multiplicar.R
@@ -0,0 +1,15 @@
+#'Função para multiplicar
+#'
+#'Esta função será utilizada para multiplicar dois numeros
+#'
+#' @param a um numero
+#' @param b outro numero
+#'
+#' @examples
+#' multiplicar(3, 4)
+#'
+#'
+#' @export
+multiplicar<-function(a, b){
+  a * b
+}
